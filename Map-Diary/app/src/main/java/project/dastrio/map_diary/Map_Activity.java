5b47f389c92e03e3ba426ed8719857b1647aed16
Noeggu <noel.knubel@bbcag.ch>
2015-06-19 10:49:50 +0200

    Added Google Maps API

--- Map-Diary/app/src/main/java/project/dastrio/map_diary/Map_Activity.java
+++ Map-Diary/app/src/main/java/project/dastrio/map_diary/Map_Activity.java
@@ -1,41 +1,23 @@
 package project.dastrio.map_diary;
 
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.util.Map;
 
 
-public class Login_Activity extends ActionBarActivity {
+public class Map_Activity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(listener);
+        setContentView(R.layout.activity_map);
     }
-
-    View.OnClickListener listener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(getApplicationContext(), Map_Activity.class);
-            startActivity(intent);
-        }
-    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        getMenuInflater().inflate(R.menu.menu_map, menu);
         return true;
     }
 
